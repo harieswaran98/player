1f495a6730e8a3585b4d91a9dd3e6d0f022addd0
harieswaran98 <46838567+harieswaran98@users.noreply.github.com>
2019-03-26 08:39:21 +0530

    Create 21.C

---
+++ 21.C
@@ -0,0 +1,12 @@
+int main()
+{
+	int a,b,c,d,e,f;
+	
+	scanf("%d %d\n",&a,&b);
+	scanf("%d %d\n",&c,&d);
+	scanf("%d %d",&e,&f);
+	if(((a==c)&&(a==e))||((b==d)&&(b==f)))
+	printf("yes");
+	else
+	printf ("no");
+	}
