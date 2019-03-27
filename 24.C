4839f5f142821c03baefb411ca031d00ed10f0c9
harieswaran98 <46838567+harieswaran98@users.noreply.github.com>
2019-03-27 08:12:28 +0530

    Create 24.C

---
+++ 24.C
@@ -0,0 +1,20 @@
+int main()
+{
+	int a,b,c=0,d,i;
+	char e[100];
+	scanf("%s",&e);
+	for(i=0;e[i]!='\0';i++)
+	{
+	     if((e[i]>=48)&&(e[i]<=57))
+	     {
+	     	}
+	     else
+	     {
+	     c=1; //printf("%d",c);
+	     	}
+	     	}
+	     if(c==1)
+	     	printf("no");
+	     else
+	     	printf("yes");
+	     }
